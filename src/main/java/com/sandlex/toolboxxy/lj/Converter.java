ce7f203b2f163a8c1e791eecd7d8d0ccc0957c0b
sandlex <me@sandlex.com>
2018-12-22 01:21:57 +0100

    Conversion from lj export month files into markdown file per post

--- src/main/java/com/sandlex/toolboxxy/lj/Converter.java
+++ src/main/java/com/sandlex/toolboxxy/lj/Converter.java
@@ -3,36 +3,73 @@ package com.sandlex.toolboxxy.lj;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ * Converter of lj export files into markdown.
+ */
 public class Converter {
 
     public static void main(String[] args) throws IOException {
-        File dir = new File(args[0]);
+        String sourceDir = args[0];
+        File dir = new File(sourceDir);
 
         CsvMapper mapper = new CsvMapper();
         mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
 
         Files.list(dir.toPath())
-                .forEach(path -> {
+                .filter(path -> path.toFile().isFile())
+                .forEach(sourceFile -> {
                     try {
-                        parseFile(mapper, path);
+                        parseFile(mapper, sourceDir, sourceFile);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 });
     }
 
-    private static void parseFile(CsvMapper mapper, Path file) throws IOException {
-        MappingIterator<String[]> it = mapper.readerFor(String[].class).readValues(file.toFile());
+    private static void parseFile(CsvMapper mapper, String sourceDir, Path sourceFile) throws IOException {
+        CsvSchema schema = CsvSchema.builder().setSkipFirstDataRow(true).build();
+        MappingIterator<String[]> it = mapper
+                .readerFor(String[].class)
+                .with(schema)
+                .readValues(new InputStreamReader(new FileInputStream(sourceFile.toFile()), StandardCharsets.UTF_8));
         Iterable<String[]> iterable = () -> it;
         Stream<String[]> targetStream = StreamSupport.stream(iterable.spliterator(), false);
-        targetStream.forEach(post -> System.out.println(post[0]));
+        targetStream.forEach(post -> {
+            File nf = new File(sourceDir + "/md/" + post[0] + ".md");
+            try {
+                Path f = Files.createFile(nf.toPath());
+                byte[] buf = getFileContent(post).getBytes();
+                Files.write(f, buf);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
+
+    private static String getFileContent(String[] input) {
+        String separator = System.lineSeparator();
+        return String.format(
+                "+++" + separator +
+                        "title = \"%s\"" + separator +
+                        "date = \"%s\"" + separator +
+                        "tags = []" + separator +
+                        "music = \"%s\"" + separator +
+                        "mood = \"%s\"" + separator +
+                        "+++" + separator + separator +
+                        "%s", input[2], input[1].replace(" ", "T"),
+                input[5], input[6], input[3]);
+    }
+
 }
